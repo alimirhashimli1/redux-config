d28cefd259debe098d6cf058ef9231c138f346b5
alimirhashimli <alimirhashimli@gmail.com>
2023-12-14 02:57:28 +0100

    fixed the userId

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import AddPostForm from "./features/posts/AddPostForm";
-import AddPostFormPractice from "./features/posts/AddPostFormPractice";
 import PostsList from "./features/posts/PostsList";
-import PostsListPractice from "./features/posts/PostsListPractice";
 
 function App() {
   return (
     <div className="App">
-      <PostsListPractice />
-      <AddPostFormPractice />
+      <PostsList />
+      <AddPostForm />
     </div>
   );
 }
